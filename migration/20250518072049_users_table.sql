8d5db206123de779e2763c301a58fbbed8559612
Sayeed <sayeedulhoda@gmail.com>
2025-05-18 13:33:27 +0530

    table user, session updated and created btree index on user_id in session

---
+++ migration/20250518072049_users_table.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS users(
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(300) NOT NULL UNIQUE,
+  name VARCHAR(300) NOT NULL,
+  password_hash VARCHAR(300) NOT NULL,
+  email_verified BOOL NOT NULL DEFAULT FALSE,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS sessions;
+-- +goose StatementEnd
--- migration/20250518072100_session_table.sql
+++ migration/20250518072100_session_table.sql
@@ -2,17 +2,13 @@
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS sessions(
   id SERIAL PRIMARY KEY,
-  user_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   token TEXT NOT NULL,
   expires_at TIMESTAMP WITH TIME ZONE,
   ip_address VARCHAR(255) NOT NULL,
   user_agent VARCHAR(1024) NOT NULL,
   created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-  updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-  CONSTRAINT fk_user
-    FOREIGN KEY(user_id)
-    REFERENCES users(id) 
-    ON DELETE CASCADE
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
 -- +goose StatementEnd
 
